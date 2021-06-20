70c5f615b3182b3b6313f44b7106c06fd035c56f
Oleg <oi@atticlab.net>
2021-06-20 19:37:45 +0300

    change static page to remove any dependencies

---
+++ static/plot-control.js
@@ -0,0 +1,148 @@
+async function fetchData(by) {
+  const res = await fetch(`/get?last=${by}`)
+  const weather = await res.json()
+
+  if (weather.length <= 0) {
+    throw new Error('empty data')
+  }
+  const temp = weather.map(w => w.temp)
+  const humidity = weather.map(w => w.humidity)
+  const preasure = weather.map(w => w.preasure)
+  const time = weather.map(w => (new Date(w.time)).getTime())
+
+  function timeSeparator(timeArray) {
+    const firstTime = Math.min(...timeArray)
+    const lastTime = Math.max(...timeArray)
+    const minutesCount = (lastTime  - firstTime) / 1000 / 60
+
+    let separations = []
+    if (minutesCount < 60){ // if less then hour, get all minutes
+      let temp = Math.ceil(firstTime / 1000 / 60)
+      const lastMin = Math.floor(lastTime / 1000 / 60)
+      while (temp <= lastMin) {
+        separations.push(temp * 60 * 1000)
+        temp++
+      }
+    } else if (minutesCount < 60 * 60) { // if less then day, get all hours
+      let temp = Math.ceil(firstTime / 1000 / 60 / 60)
+      const lastHour = Math.floor(lastTime / 1000 / 60 / 60)
+      while (temp <= lastHour) {
+        separations.push(temp * 60  *  60 * 1000)
+        temp++
+      }
+    } else if (minutesCount < 60 * 60 * 24) { // if less then month, get all days
+      let temp = Math.ceil(firstTime / 1000 / 60 / 60 / 24)
+      const lastDay = Math.floor( (lastTime / 1000 / 60) / 60 / 24)
+      // Account timezones
+      const timezoneShift = (new Date().getTimezoneOffset()) * 60 * 1000
+      while(temp <= lastDay) {
+        separations.push(temp * 24 * 60 * 60 * 1000 + timezoneShift)
+        temp++
+      }
+    }
+    return separations
+  }
+  const data = {
+    xAxis: time,
+    xSeparator: timeSeparator(time),
+    yAxises: [{
+      name: "Temperature",
+      color: 'rgb(255, 102, 102)',
+      values: temp,
+    }, {
+      name: "Humidity",
+      color: 'rgb(153, 102, 255)',
+      values: humidity,
+    }, {
+      name: "Preasure",
+      color: 'rgb(0, 153, 255)',
+      values: preasure,
+    }]
+  }
+  return data
+}
+
+;(async function main() {
+  // Get start data
+  let data = await fetchData('day')
+
+  const lastTemp = document.getElementById('last-temp')
+  const lastHumidity = document.getElementById('last-humidity')
+  const lastPreasure = document.getElementById('last-preasure')
+  const lastTime = document.getElementById('last-time')
+
+  const selectedTemp = document.getElementById('selected-temp')
+  const selectedHumidity = document.getElementById('selected-humidity')
+  const selectedPreasure = document.getElementById('selected-preasure')
+  const selectedTime = document.getElementById('selected-time')
+
+  const ys = data.yAxises
+  lastTemp.innerText = String(ys[0].values[ys[0].values.length - 1]) + ' °C'
+  lastHumidity.innerText = String(ys[1].values[ys[1].values.length - 1]) + ' %'
+  lastPreasure.innerText = String(ys[2].values[ys[2].values.length - 1]) + ' mBar'
+  lastTime.innerText = (new Date(data.xAxis[data.xAxis.length - 1])).toLocaleString('en-GB')
+
+  selectedTemp.innerText = lastTemp.innerText
+  selectedHumidity.innerText = lastHumidity.innerText
+  selectedPreasure.innerText = lastPreasure.innerText
+  selectedTime.innerText = lastTime.innerText
+
+  // Define cursor selectring behavior
+  function handleFeedback(selected) {
+    const { xAxis, yAxises } = selected
+
+    selectedTemp.innerText = String(yAxises[0].value) + ' °C'
+    selectedHumidity.innerText = String(yAxises[1].value) + ' %'
+    selectedPreasure.innerText = String(yAxises[2].value) + ' mBar'
+    selectedTime.innerText = new Date(xAxis).toLocaleString('en-GB')
+  }
+
+  // Draw plot in canvas
+  const plot = new Plot(document.getElementById('plot'), data, handleFeedback)
+
+  // Define period selectors
+  const selectPeriod = document.getElementById('select-period')
+  for (let periodName of ['3hours', 'day', '3days', 'week', 'mounth']) {
+    const periodOption = document.createElement('div')
+
+    const periodInput = document.createElement('input')
+    periodInput.setAttribute('type', 'radio')
+    periodInput.setAttribute('name', 'last-period')
+    periodInput.setAttribute('value', periodName)
+    periodInput.setAttribute('id', periodName)
+    periodInput.setAttribute('style', 'margin: .4rem;')
+    periodInput.addEventListener('change', async function(e) {
+      data = await fetchData(e.target.value)
+      plot.reDraw(data)
+    })
+    const periodLabel = document.createElement('label')
+    periodLabel.setAttribute('for', periodName)
+    periodLabel.innerText = periodName
+
+    periodOption.appendChild(periodInput)
+    periodOption.appendChild(periodLabel)
+    selectPeriod.appendChild(periodOption)
+  }
+
+  document.getElementById('isTemp').onclick = async function() {
+    plot.excluded = plot.excluded.filter(v => v !== "Temperature")
+    if (!this.checked){
+      plot.excluded.push("Temperature")
+    }
+    plot.reDraw()
+  }
+  document.getElementById('isHumidity').onclick = async function() {
+    plot.excluded = plot.excluded.filter(v => v !== "Humidity")
+    if (!this.checked){
+      plot.excluded.push("Humidity")
+    }
+    plot.reDraw()
+  }
+  document.getElementById('isPreasure').onclick = async function() {
+    plot.excluded = plot.excluded.filter(v => v !== "Preasure")
+    if (!this.checked){
+      plot.excluded.push("Preasure")
+    }
+    plot.reDraw()
+  }
+})()
--- static/plot.js
+++ static/plot.js
@@ -1,159 +1,154 @@
+class Plot {
+  constructor(canvasElem, data, feedback) {
+    this.plot = canvasElem
+    this.height = canvasElem.height
+    this.ctx = canvasElem.getContext('2d')
+    this.excluded = []
+    
+    this.plot.addEventListener('mousemove', e => {
+      this.sendFeedback(e.offsetX)
+    })
+    this.plot.addEventListener('touchmove', e => {
+      this.sendFeedback(e.touches[0].pageX)
+    })
+    this.reDraw(data, feedback)
+  }
 
-const tChartElem = document.getElementById('tempChart')
-const tColor = 'rgb(255, 102, 102)'
+  sendFeedback(x) {
+    if (x < 0) {
+      x = 0
+    } else if (this.plot.width < x) {
+      x = this.plot.width
+    }
+    const target = this.startX + x / this.scaleX
 
-const hChartElem = document.getElementById('humidityChart')
-const hColor = 'rgb(153, 102, 255)'
-
-const pChartElem = document.getElementById('presureChart')
-const pColor = 'rgb(0, 153, 255)'
-
-
-const filterThreeHours = document.getElementById('filter-last-3-hours')
-const filterDay = document.getElementById('filter-last-day')
-const filterThreeDays = document.getElementById('filter-last-3-day')
-const filterWeek = document.getElementById('filter-last-week')
-const filterMonth = document.getElementById('filter-month')
-
-const filterDescription = document.getElementById('filter-description')
-
-const filters = [
-  filterThreeHours, 
-  filterDay, 
-  filterThreeDays, 
-  filterWeek, 
-  filterMonth,
-]
-
-async function updateLastData(by){
-  function createConfig(data) {
-    return {
-      type: 'line',
-      data,
-      options: {
-        // responsive: false,
-        interaction: {
-          mode: 'index',
-          intersect: false,
-        },
+    function findClosest(arr, target) {
+      let n = arr.length
+      if (target <= arr[0]){
+        return 0
       }
+      if (target >= arr[n - 1]){
+        return n - 1
+      }
+      let i = 0, j = n, mid = 0
+      while (i < j) {
+        mid = Math.round((i + j) / 2)
+        if (arr[mid] == target) {
+          return mid
+        }
+        if (target < arr[mid]) {
+          if (mid > 0 && target > arr[mid - 1]) {
+            if (target - arr[mid - 1] >= arr[mid] - target)
+              return mid
+            else
+              return mid - 1
+          }
+          j = mid
+        } else {
+          if (mid < n - 1 && target < arr[mid + 1]){
+            if (target - arr[mid] >= arr[mid + 1] - target)
+              return mid + 1
+            else
+              return mid
+          }
+          i = mid + 1
+        }
+      }
+      return mid
     }
-  }
-  const res = await fetch(`/get?last=${by}`)
-  const weather = await res.json()
+    
+    const closestX = findClosest(this.x, target)
+    const nowX = (this.x[closestX] - this.startX) * this.scaleX
 
-  if (weather.length <= 0 ) {
-    throw new Error('empty data')
-  }
-  const temp = weather.map(w => w.temp)
-  const humidity = weather.map(w => w.humidity)
-  const preasure = weather.map(w => w.preasure)
+    this.ctx.clearRect(0, 0, this.plot.width, this.height)
+    this.ctx.putImageData(this.savedGraph, 0, 0)
 
-  const time = weather.map(w => {
-    const date = new Date(w.time)
-    if (by === 'day' || by === '3hour'){
-      return date.toLocaleTimeString('en-GB')
+    const selected = {
+      xAxis: this.x[closestX],
+      yAxises: [],
     }
-    return date.toLocaleString('en-GB')
-  })
+    for (let y of this.ys) {
+      selected.yAxises.push({
+        name: y.name,
+        value: y.values[closestX],
+      })
+      if (this.excluded.includes(y.name)) {
+        continue
+      }
+      const positionY = (y.values[closestX] - Math.min(...y.values)) / (Math.max(...y.values) - Math.min(...y.values))
+      const nowY = this.height * (1 - positionY)
+      this.ctx.fillStyle = y.color
+      this.ctx.beginPath()
+      this.ctx.arc(nowX, nowY, 5, 0, 7)
+      this.ctx.fill()
+    }
+    this.feedback(selected)
+  }
 
-  const lastTime = new Date(weather[weather.length - 1].time)
-  const lastData = `Temperature: \t${weather[weather.length - 1].temp} °C
-                    Humidity: \t${weather[weather.length - 1].humidity} %
-                    Preasure: \t${weather[weather.length - 1].preasure} mBar
-                    Time: \t${lastTime.toLocaleTimeString('en-GB')}`
-  document.getElementById('lastData').innerText = lastData
+  drawGraph() {
+    this.ctx.clearRect(0, 0, this.plot.width, this.height)
+    const x = this.x
+    const ys = this.ys
 
-  const tChart = new Chart(tChartElem, createConfig({
-    labels: time,
-    datasets: [{
-      label: 'Temperature',
-      backgroundColor: tColor,
-      borderColor: tColor,
-      data: temp,
-    }]
-  }))
-  const hChart = new Chart(hChartElem, createConfig({
-    labels: time,
-    datasets: [{
-      label: 'Humidity',
-      backgroundColor: hColor,
-      borderColor: hColor,
-      data: humidity,
-    }]
-  }))
-  const pChart = new Chart(pChartElem, createConfig({
-    labels: time,
-    datasets: [{
-      label: 'Preasure',
-      backgroundColor: pColor,
-      borderColor: pColor,
-      data: preasure,
-    }]
-  }))
-  return [tChart, hChart, pChart]
+    this.startX = x[0]
+    this.scaleX = this.plot.width / (x[x.length - 1] - x[0])
+
+    for (let y of ys) {
+      if (this.excluded.includes(y.name)) {
+        continue
+      }
+      const startY = Math.min(...y.values)
+      const scaleY = this.height / (Math.max(...y.values) - Math.min(...y.values))
+
+      this.ctx.beginPath()
+      for (let i = 1; i < y.values.length; i++) {
+        const prevX = (x[i - 1] - this.startX) * this.scaleX
+        const prevY = (y.values[i - 1] - startY) * scaleY
+
+        const nowX = (x[i] - this.startX) * this.scaleX
+        const nowY = (y.values[i] - startY) * scaleY
+
+        this.ctx.moveTo(prevX, this.height - prevY)
+        this.ctx.lineTo(nowX, this.height - nowY)
+        this.ctx.arc(nowX, this.height - nowY, 1, 0, 7)
+        this.ctx.strokeStyle = y.color
+      }
+      this.ctx.stroke()
+    }
+
+    // Draw separations
+    this.ctx.beginPath()
+    for (let separation of this.xSeparator) {
+      separation = (separation - this.startX) * this.scaleX
+      this.ctx.moveTo(separation, 0)
+      this.ctx.lineTo(separation, this.height)
+      this.ctx.strokeStyle = 'rgb(160,160,160)'
+    }
+    this.ctx.stroke()
+
+    this.savedGraph = this.ctx.getImageData(0, 0, this.plot.width, this.height)
+  }
+
+  reDraw(data, feedback){
+    if (typeof data !== "undefined") {
+      let { xAxis, xSeparator, yAxises } = data
+      for (let y of yAxises) {
+        if (y.values.length < 1) {
+          console.log(y.values.length)
+          throw new Error('yAxis length less then 1')
+        }
+        if (xAxis.length !== y.values.length) {
+          console.log(xAxis.length, y.values.length)
+          throw new Error('yAxis length not equal to xAxis')
+        }
+      }
+      this.x = xAxis
+      this.xSeparator = xSeparator
+      this.ys = yAxises
+    }
+    if (typeof feedback !== "undefined") {
+      this.feedback = feedback
+    }
+    this.drawGraph()
+  }
 }
-
-;(async function main() {
-  let charts = await updateLastData('day')
-
-  filterThreeHours.onclick = async function() {
-    if (this.classList.contains('active')){
-      return
-    }
-    filters.map(f => f.classList.remove('active'))
-    this.classList.add('active')
-    charts.map(ch => ch.destroy())
-
-    charts = await updateLastData('3hours')
-    filterDescription.innerText = 'Data by 3 Hours'
-  }
-  filterDay.onclick = async function() {
-    if (this.classList.contains('active')){
-      console.log('active')
-      return
-    }
-    filters.map(f => f.classList.remove('active'))
-    this.classList.add('active')
-    charts.map(ch => ch.destroy())
-
-    charts = await updateLastData('day')
-    filterDescription.innerText = 'Data by Day'
-  }
-  filterThreeDays.onclick = async function() {
-    if (this.classList.contains('active')){
-      console.log('active')
-      return
-    }
-    filters.map(f => f.classList.remove('active'))
-    this.classList.add('active')
-    charts.map(ch => ch.destroy())
-
-    charts = await updateLastData('3days')
-    filterDescription.innerText = 'Data by 3 Day'
-  }
-  filterWeek.onclick = async function() {
-    if (this.classList.contains('active')){
-      console.log('active')
-      return
-    }
-    filters.map(f => f.classList.remove('active'))
-    this.classList.add('active')
-    charts.map(ch => ch.destroy())
-
-    charts = await updateLastData('week')
-    filterDescription.innerText = 'Data by Week'
-  }
-  filterMonth.onclick = async function() {
-    if (this.classList.contains('active')){
-      console.log('active')
-      return
-    }
-    filters.map(f => f.classList.remove('active'))
-    this.classList.add('active')
-    charts.map(ch => ch.destroy())
-
-    charts = await updateLastData('month')
-    filterDescription.innerText = 'Data by Month'
-  }
-})()
