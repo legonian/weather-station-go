8ae3afa36f2b3fdcc0085d71de4c76e64405953c
Oleg <oi@atticlab.net>
2021-06-26 19:40:19 +0300

    remove use of log file

--- main.go
+++ main.go
@@ -1,21 +1,20 @@
 package main
 
 import (
-	"io"
 	"legonian/weather/api"
 	"legonian/weather/config"
 	"legonian/weather/db"
 	"log"
-	"os"
 )
 
 func main() {
-	logFile, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	if err != nil {
-		log.Fatalf("error opening file: %v", err)
-	}
-	defer logFile.Close()
-	log.SetOutput(io.MultiWriter(os.Stdout, logFile))
+	// Uncomment to also save logs to log file
+	// logFile, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	// if err != nil {
+	// 	log.Fatalf("error opening file: %v", err)
+	// }
+	// defer logFile.Close()
+	// log.SetOutput(io.MultiWriter(os.Stdout, logFile))
 
 	cfg, err := config.New("config.json")
 	if err != nil {
